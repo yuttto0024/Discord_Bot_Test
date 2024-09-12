9d22bc67203ed6b177f1dd1497859911136f8934
unidon222 <takasi.0951@gmail.com>
2024-09-12 15:00:51 +0900

    feat:ボイスチャンネルの滞在時間を特定のテキストチャンネルに投稿する機能を実装

--- main.go
+++ main.go
@@ -1,44 +1,81 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 
-	"github.com/bwmarrin/discordgo"
+    "github.com/bwmarrin/discordgo"
 )
 
+// ボイスチャンネルへの参加時間を記録するためのマップ
+var voiceStates = make(map[string]time.Time)
+
+// 監視するボイスチャンネルのID（実際のIDに置き換えてください）
+var targetVoiceChannelID = "1278707009549631553"
+
+// メッセージを投稿するテキストチャンネルのID（実際のIDに置き換えてください）
+var targetTextChannelID = "1278707009549631552"
+
 func main() {
-	// 環境変数からトークンを取得
-	token := os.Getenv("DISCORD_TOKEN")
-	if token == "" {
-		log.Fatal("DISCORD_TOKEN is not set")
-	}
+    // 環境変数からDiscord Botのトークンを取得
+    token := os.Getenv("DISCORD_TOKEN")
 
-	dg, err := discordgo.New("Bot " + token)
-	if err != nil {
-		fmt.Println("Error creating Discord session,", err)
-		return
-	}
+    // Discordセッションを作成
+    dg, err := discordgo.New("Bot " + token)
+    if err != nil {
+        fmt.Println("Error creating Discord session,", err)
+        return
+    }
 
-	dg.AddHandler(messageCreate)
+    // ボイスチャンネルの状態が変わったときに呼ばれるハンドラを追加
+    dg.AddHandler(voiceStateUpdate)
 
-	err = dg.Open()
-	if err != nil {
-		fmt.Println("Error opening connection,", err)
-		return
-	}
+    // Discordへの接続を開始
+    err = dg.Open()
+    if err != nil {
+        fmt.Println("Error opening connection,", err)
+        return
+    }
 
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	select {}
+    fmt.Println("Bot is now running. Press CTRL-C to exit.")
+
+    // プログラムが終了しないように待機
+    stop := make(chan os.Signal, 1)
+    signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+    <-stop
+
+    // プログラム終了時にDiscordセッションを閉じる
+    dg.Close()
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
+// ボイスチャンネルの状態が更新されたときに呼ばれる関数
+func voiceStateUpdate(s *discordgo.Session, vsu *discordgo.VoiceStateUpdate) {
+    userID := vsu.UserID
 
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
+    // ユーザーがボイスチャンネルに参加したとき
+    if vsu.BeforeUpdate == nil && vsu.ChannelID == targetVoiceChannelID {
+        // 参加した時間を記録
+        voiceStates[userID] = time.Now()
+    } else if vsu.BeforeUpdate != nil && vsu.BeforeUpdate.ChannelID == targetVoiceChannelID && vsu.ChannelID != targetVoiceChannelID {
+        // ユーザーがボイスチャンネルを退出したとき
+        if joinTime, ok := voiceStates[userID]; ok {
+            // 滞在時間を計算
+            duration := time.Since(joinTime)
+            delete(voiceStates, userID)
+
+            // 時間、分、秒を計算
+            hours := int(duration.Hours())
+            minutes := int(duration.Minutes()) % 60
+            seconds := int(duration.Seconds()) % 60
+
+            // 滞在時間をフォーマットしてメッセージを作成
+            message := fmt.Sprintf("<@%s> stayed in the voice channel for %02d:%02d:%02d", userID, hours, minutes, seconds)
+
+            // テキストチャンネルにメッセージを送信
+            s.ChannelMessageSend(targetTextChannelID, message)
+        }
+    }
 }
