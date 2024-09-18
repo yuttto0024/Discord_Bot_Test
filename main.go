13f0f43651434b27411088a8a9f49e5857cc30f8
yuttto0024 <yuto0024@outlook.com>
2024-09-18 16:02:33 +0900

    improve

--- main.go
+++ main.go
@@ -4,51 +4,69 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv" // .envファイルを読み込むために追加
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    // .envファイルを読み込み
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatalf(".envファイルの読み込みに失敗しました: %v", err)
-    }
+	// .envファイルを読み込み
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf(".envファイルの読み込みに失敗しました: %v", err)
+	}
 
-    // 環境変数からDiscord Botのトークン取得
-    token := os.Getenv("DISCORDTOKEN")
-    if token == "" {
-        log.Fatal("Discordトークンが設定されていません。環境変数DISCORDTOKENを設定してください。")
-    }
+	// 環境変数からDiscord Botのトークン取得
+	token := os.Getenv("DISCORDTOKEN")
+	if token == "" {
+		log.Fatal("Discordトークンが設定されていません。環境変数DISCORDTOKENを設定してください。")
+	}
 
-    // 新しいDiscordセッションを作成
-    dg, err := discordgo.New("Bot " + token)
-    if err != nil {
-        log.Fatalf("Error creating Discord session: %v", err)
-    }
+	// 新しいDiscordセッションを作成
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatalf("Error creating Discord session: %v", err)
+	}
 
-    // Botを起動し、Discordのサーバーに接続
-    err = dg.Open()
-    if err != nil {
-        log.Fatalf("Error opening connection: %v", err)
-    }
-    fmt.Println("Bot is now running. Press CTRL+C to exit")
+	// ボイスチャンネルの入退出をリッスンするためのイベントハンドラを登録
+	dg.AddHandler(voiceStateUpdate)
 
-    // 定期的にメッセージを送信するためのタイマー
-    ticker := time.NewTicker(1 * time.Minute) // 1分ごとにメッセージを送信
-    defer ticker.Stop()
+	// Botを起動し、Discordのサーバーに接続
+	err = dg.Open()
+	if err != nil {
+		log.Fatalf("Error opening connection: %v", err)
+	}
+	fmt.Println("Bot is now running. Press CTRL+C to exit")
 
-    // メッセージを送信するテキストチャンネルのID
-    channelID := "1278707009549631552"
-
-    // 1分ごとに「Good job!!」を送信
-    for {
-        <-ticker.C
-        _, err := dg.ChannelMessageSend(channelID, "Good job!!")
-        if err != nil {
-            log.Printf("Error sending message: %v", err)
-        }
-    }
+	// プログラムが終了するまで待機
+	select {}
 }
+
+// ボイスチャンネルの状態が更新されたときに呼ばれるイベントハンドラ
+func voiceStateUpdate(s *discordgo.Session, vsu *discordgo.VoiceStateUpdate) {
+	if vsu == nil {
+		log.Println("VoiceStateUpdate event is nil")
+		return
+	}
+
+	// ユーザーがボイスチャンネルから退出したときの処理
+	// 状態が変わった後のチャンネルIDが空である場合
+	if vsu.BeforeUpdate != nil && vsu.ChannelID == "" {
+		// 退出したユーザーの情報
+		userID := vsu.UserID
+		channelID := "1278707009549631552" // メッセージを送信するチャンネルのID
+
+		// 退出したユーザーの情報をログに出力
+		log.Printf("User %s has left the voice channel", userID)
+
+		// メッセージを送信
+		message := "Good job!!"
+		_, err := s.ChannelMessageSend(channelID, message)
+		if err != nil {
+			log.Printf("Error sending message: %v", err)
+		}
+	}
+}
+
+
+
